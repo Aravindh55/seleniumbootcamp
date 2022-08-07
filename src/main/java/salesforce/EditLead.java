7f42b845c31c01dc8500f0965e3c7ef0637c4212
Aravindh <aravindhraja.ra@gmail.com>
2022-08-07 12:30:04 +0530

    second commit

--- src/main/java/salesforce/EditLead.java
+++ src/main/java/salesforce/EditLead.java
@@ -3,6 +3,8 @@ package salesforce;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -33,5 +35,38 @@ public class EditLead {
 	driver.findElement(By.xpath("//button[contains(text(),'View')]")).click();
 	driver.findElement(By.xpath("//input[contains(@placeholder,'s')]")).sendKeys("Leads");
 	driver.findElement(By.xpath("//p[contains(@class,'s')]")).click();
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='Lead-search-input']")));
+	driver.findElement(By.xpath("//input[@name='Lead-search-input']")).sendKeys("Kumar");
+	driver.findElement(By.xpath("//input[@name='Lead-search-input']")).sendKeys(Keys.ENTER);
+	Thread.sleep(4000);
+    WebElement findElement = driver.findElement(By.xpath("(//span[contains(@class,'utility-down')])[1]"));
+    driver.executeScript("arguments[0].scrollIntoView();", findElement);
+	Thread.sleep(5000);
+	findElement.click();
+	driver.findElement(By.xpath("//a[@title='Edit']")).click();
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='firstName']")));
+	driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Ganesh");
+	WebElement findElement2 = driver.findElement(By.xpath("(//button[contains(@aria-haspopup,'list')])[4]"));
+	driver.executeScript("arguments[0].scrollIntoView();", findElement2);
+	Thread.sleep(3000);
+	findElement2.click();
+	driver.findElement(By.xpath("//span[contains(@title,'W')]")).click();
+	driver.findElement(By.xpath("//button[contains(@name,'SaveE')]")).click();
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[contains(@class,'forceInlineEdit')]/a)[1]")));
+	Thread.sleep(5000);
+	driver.findElement(By.xpath("(//span[contains(@class,'forceInlineEdit')]/a)[1]")).click();
+	Thread.sleep(5000);
+	String text = driver.findElement(By.xpath("//lightning-formatted-name[@slot='primaryField']")).getText();
+	if (text.contains("Ganesh")) {
+		System.out.println("The Leads is editted successfully");
+		
+	}else {
+		throw new RuntimeException();
+	}
+	
+	
+	
+	
+	
 	}
 }
